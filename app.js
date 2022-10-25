4c0ab1220a5c6a6464985cc92585a5e104071ecd
Thomas McLaughlin <123tommygun1@gmail.com>
2022-10-25 14:26:18 -0400

    moving onto extra credit

--- app.js
+++ app.js
@@ -6,9 +6,70 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static('public'))
 
+// const id = req.params.id;
+// const post = postBank.find(id);
+
+app.get("/posts/:id",  (req, res) => {
+ 
+  const id = req.params.id;
+const post = postBank.find(id);
+const placeholder = req.params
+console.log(req.params)
+
+if (!post.id) {
+  res.status(404)
+  const html = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <header><img src="/logo.png"/>Wizard News</header>
+    <div class="not-found">
+      <p>Accio Page! 🧙‍♀️ ... Page Not Found</p>
+      <img src="/dumbledore-404.gif" />
+    </div>
+  </body>
+  </html>`
+  res.send(html)
+} else  {
+
+const html = `<!DOCTYPE html>
+<html>
+<head>
+  <title>Wizard News</title>
+  <link rel="stylesheet" href="/style.css" />
+</head>
+<body>
+  <div class="news-list">
+    <header><img src="/logo.png"/>Wizard News</header>
+   
+      <div class='news-item'>
+        <p>
+          <span class="news-position">${post.title}. ▲</span>${post.name}
+          <small>(by ${post.content})</small>
+        </p>
+        <small class="news-info">
+          ${post.upvotes} upvotes | ${post.date}
+        </small>
+      </div>
+    )
+  </div>
+</body>
+</html>`;
+
+res.send(html);
+}
+})
+
+///////
+
 app.get("/",  (req, res) => {
 
 const posts = postBank.list()
+
 
 const html = `<!DOCTYPE html>
 <html>
@@ -22,7 +83,8 @@ const html = `<!DOCTYPE html>
     ${posts.map(post => `
       <div class='news-item'>
         <p>
-          <span class="news-position">${post.id}. ▲</span>${post.title}
+          <span class="news-position">${post.id}. ▲</span>
+          <a href="/posts/${post.id}">${post.title}</a>
           <small>(by ${post.name})</small>
         </p>
         <small class="news-info">
@@ -33,6 +95,7 @@ const html = `<!DOCTYPE html>
   </div>
 </body>
 </html>`;
+
 
 res.send(html);
 })
